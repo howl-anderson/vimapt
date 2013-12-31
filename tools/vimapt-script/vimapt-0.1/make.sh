8baf9301ab00abce2a0c1fd7ad700d9037cc8730
howlanderson <u1mail2me@gmail.com>
2014-01-01 01:42:26 +0800

    vimapt-tools and vimapt-script will combine to one
    this is a importment change. vimapt-tools and vimapt-script
    will combine to vimapt.

--- tools/vimapt-script/vimapt-0.1/make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm ../vimapt_0.1.orig.tar.gz
-python setup.py bdist_egg
-dh_make --createorig
-debuild
-sudo apt-get purge python-vimapt
-sudo dpkg -i ../python-vimapt_0.1-1_all.deb
--- tools/vimapt-tools/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm vimapt-tools_*
-cd vimapt-tools-0.1
-dh_make --createorig
-debuild
-cd ..
-sudo apt-get purge vimapt-tools
-sudo dpkg -i vimapt-tools_0.1-1_all.deb
