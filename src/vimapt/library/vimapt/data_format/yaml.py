7e766747dbda4548b63b278e062335c8a10fe008
Xiaoquan Kong <xiaoquan.kong@ele.me>
2017-04-18 01:22:14 +0800

    Use PyYAML as YAML's loader and dumper

--- src/vimapt/library/vimapt/data_format/yaml.py
+++ src/vimapt/library/vimapt/data_format/yaml.py
@@ -1,4 +1,10 @@
-from pureyaml import dump as dumps
-from pureyaml import load as loads
+from __future__ import absolute_import
+
+import functools
+
+from yaml import dump, Dumper, load, Loader
+
+dumps = functools.partial(dump, Dumper=Dumper)
+loads = functools.partial(load, Loader=Loader)
 
 __all__ = ['dumps', 'loads']
