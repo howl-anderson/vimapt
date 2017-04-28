fb003070572b22fc0b0c93ccfe5f983cb435dcb0
Xiaoquan Kong <u1mail2me@gmail.com>
2017-04-28 15:47:38 +0800

    Catch exception for fail message

--- src/vimapt/bin/install.py
+++ src/vimapt/bin/install.py
@@ -1,16 +1,20 @@
-#!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 import sys
 
 from vimapt import Install
+from vimapt.exception import VimaptAbortOperationException
 
 
 def main():
     vim_dir = sys.argv[1]
     package_name = sys.argv[2]
     install = Install.Install(vim_dir)
-    install.repo_install(package_name)
-    print("Install Succeed!")
+    try:
+        install.repo_install(package_name)
+    except VimaptAbortOperationException as e:
+        print(e)
+    else:
+        print("Install Succeed!")
 
 
 if __name__ == "__main__":
