1d6709ffca4ff48f9636718fb5d75bf7345c4191
Xiaoquan Kong <u1mail2me@gmail.com>
2017-05-03 14:30:24 +0800

    change log file location

--- src/vimapt/library/vimapt/__init__.py
+++ src/vimapt/library/vimapt/__init__.py
@@ -1,4 +1,9 @@
 import logging
+import os
 
-logging.basicConfig(filename='/var/log/vimapt.log', level=logging.INFO)
+home_dir = os.path.expanduser('~')
+
+log_file = os.path.join(home_dir, '.vim/vimapt/log/vimapt.log')
+
+logging.basicConfig(filename=log_file, level=logging.INFO)
 logger = logging.getLogger(__name__)
