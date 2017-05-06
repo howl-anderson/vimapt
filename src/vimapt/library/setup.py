d352d6c6869acc0ebce379740d1d12d18154deda
Xiaoquan Kong <xiaoquan.kong@ele.me>
2017-05-06 13:21:51 +0800

    Update vimapt/library's package info

--- src/vimapt/library/setup.py
+++ src/vimapt/library/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='vimapt',
-    version='0.1',
+    name='vimapt_library',
+    version='0.1.0',
     install_requires=[
         'pyyaml',
         'six',
@@ -15,5 +15,5 @@ setup(
     license='GPL',
     author='Xiaoquan Kong',
     author_email='u1mail2me@gmail.com',
-    description='APT-like package manager for Vim'
+    description='library for Vimapt (APT-like package manager for Vim)'
 )
