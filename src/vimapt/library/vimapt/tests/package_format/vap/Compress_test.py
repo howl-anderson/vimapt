3941ec4578b611919eaa4c15ee3294adb1e080f5
Xiaoquan Kong <xiaoquan.kong@ele.me>
2017-05-31 11:45:39 +0800

    Fix workspace dir issue; add clean function after test

--- src/vimapt/library/vimapt/tests/package_format/vap/Compress_test.py
+++ src/vimapt/library/vimapt/tests/package_format/vap/Compress_test.py
@@ -11,10 +11,18 @@ class CompressTest(unittest.TestCase):
         
         :return: 
         """
-        workspace_dir = os.path.join(os.path.abspath(os.path.realpath(__file__)),
+        workspace_dir = os.path.join(os.path.dirname(os.path.abspath(os.path.realpath(__file__))),
                                      'test_data_for_compress')
 
         input_dir = os.path.join(workspace_dir, 'input')
         output_file = os.path.join(workspace_dir, 'output.vap')
 
-        Compress(source_dir=input_dir, output_file=output_file).compress()
+        try:
+            Compress(source_dir=input_dir, output_file=output_file).compress()
+        finally:
+            # no matter what, try to remove output file
+            try:
+                # pass
+                os.remove(output_file)
+            except OSError:
+                pass
