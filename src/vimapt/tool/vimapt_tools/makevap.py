0233f5b2169cf9b737022170b0211a910d612d32
Xiaoquan Kong <xiaoquan.kong@ele.me>
2017-06-01 06:34:02 +0800

    Add script tool for `vap` format

---
+++ src/vimapt/tool/vimapt_tools/makevap.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+
+from vimapt.package_format import get_compressor
+
+_PACKAGE_FORMAT = 'vap'
+
+
+class VimAptMakeVap(object):
+    def __init__(self, work_dir):
+        self.work_dir = work_dir
+        self.target_dir = os.path.dirname(self.work_dir)
+        self.dir_name = os.path.basename(self.work_dir)
+
+        # initial setup
+        pkg_name_segments = self.dir_name.split("_")
+        self.pkg_name = '_'.join(pkg_name_segments[:-1])
+        full_version = pkg_name_segments[-1]
+
+        self.full_pkg_name = self.pkg_name + "_" + full_version + ".vap"
+        self.target_file = os.path.join(self.target_dir, self.full_pkg_name)
+
+    def make(self):
+        compressor = get_compressor(_PACKAGE_FORMAT)
+        compress_object = compressor(self.work_dir, self.target_file)
+        compress_object.compress()
+
+
+def main():
+    obj = VimAptMakeVap(os.getcwd())
+    obj.make()
+
+if __name__ == "__main__":
+    main()
