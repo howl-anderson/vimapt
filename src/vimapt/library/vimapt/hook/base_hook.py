d995d29fdbe6b0d35a5d2bf048353ca4db559bf1
Xiaoquan Kong <xiaoquan.kong@ele.me>
2017-06-12 09:56:10 +0800

    Fix issue: BaseHook class missing a proper __init__ method

--- src/vimapt/library/vimapt/hook/base_hook.py
+++ src/vimapt/library/vimapt/hook/base_hook.py
@@ -2,6 +2,9 @@ from vimapt.exception import VimaptAbortOperationException
 
 
 class BaseHook(object):
+    def __init__(self, vim_dir):
+        self.vim_dir = vim_dir
+
     def pre_install(self):
         return True
 
@@ -23,7 +26,7 @@ class BaseHook(object):
 
         return_code = pip.main(['install', package_specification])
 
-        if return_code != 1:
+        if return_code != 0:
             msg = "Pip package {} install failed!".format(package_specification)
             raise VimaptAbortOperationException(msg)
 
@@ -36,6 +39,6 @@ class BaseHook(object):
 
         return_code = pip.main(['uninstall', package_specification])
 
-        if return_code != 1:
+        if return_code != 0:
             msg = "Pip package {} uninstall failed!".format(package_specification)
             raise VimaptAbortOperationException(msg)
