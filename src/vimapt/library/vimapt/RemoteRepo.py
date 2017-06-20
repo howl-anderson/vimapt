204f81bc0b051b0cd2a547d57c3a155e50bfe7ac
Xiaoquan Kong <xiaoquan.kong@ele.me>
2017-06-21 00:51:52 +0800

    Enhance package pool scan function to provide more detailed information

--- src/vimapt/library/vimapt/RemoteRepo.py
+++ src/vimapt/library/vimapt/RemoteRepo.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 
 import os
+import tempfile
+import logging
 
-from .data_format import dumps
+from .data_format import dumps, loads
+from vimapt.exception import (
+    VimaptException,
+    VimaptAbortOperationException,
+)
+from vimapt.package_format import get_extractor_by_detect_file
+
+logger = logging.getLogger(__name__)
 
 
 class RemoteRepo(object):
@@ -31,8 +40,49 @@ class RemoteRepo(object):
             package_name = '_'.join(pkg_name_segments[:-1])
             version_and_ext = pkg_name_segments[-1]
 
+            package_absolute_path = os.path.join(self.pool_absolute_dir, file_name)
+            meta_data = self.read_package_meta_data_by_file(package_absolute_path)
+
             version = os.path.splitext(version_and_ext)[0]
             path = os.path.join('pool/', file_name)
-            package_info = {'version': version, 'path': path}
+            package_info = {
+                'version': version,
+                'path': path,
+                'depends': meta_data.get('depends', ''),
+                'conflicts': meta_data.get('conflicts', '')
+            }
             package_data[package_name] = package_info
         return package_data
+
+    def read_package_meta_data_by_dir(self, package_dir_path):
+        """
+        Get package name by scan 'vimapt/control', used to discover the package's name only
+        :return: String of package name
+        """
+        record_dir = os.path.join(package_dir_path, 'vimapt/control')
+        meta_file_list = []
+        meta_data = None
+        for f in os.listdir(record_dir):
+            if os.path.isfile(os.path.join(record_dir, f)) and not os.path.basename(f).startswith('.'):
+                meta_file_list.append(f)
+                with open(os.path.join(record_dir, f)) as fd:
+                    meta_data = loads(fd.read())
+
+        if len(meta_file_list) != 1:
+            msg = ("Can not read package meta data: "
+                   "There are not only one meta file, "
+                   "there are {} meta files: {}.").format(len(meta_file_list), meta_file_list)
+            raise VimaptAbortOperationException(msg)
+
+        return meta_data
+
+    def read_package_meta_data_by_file(self, package_file):
+        logger.info("Start to process {}".format(package_file))
+
+        tmp_dir = tempfile.mkdtemp()
+
+        extractor = get_extractor_by_detect_file(package_file)
+        install = extractor(package_file, tmp_dir)
+        install.extract()
+
+        return self.read_package_meta_data_by_dir(tmp_dir)
