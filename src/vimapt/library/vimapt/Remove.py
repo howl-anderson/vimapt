6f6347b5d7736de13f40856dda201c2140638c9c
Xiaoquan Kong <xiaoquan.kong@ele.me>
2017-06-21 00:50:24 +0800

    Use manager package to implement remove function

--- src/vimapt/library/vimapt/Remove.py
+++ src/vimapt/library/vimapt/Remove.py
@@ -1,36 +1,17 @@
 #!/usr/bin/env python
 
-import os
+import logging
 
-from .data_format import loads
+from vimapt.manager.manage_action import ManageAction
+from vimapt.manager.manager import Manager
+
+logger = logging.getLogger(__name__)
 
 
 class Remove(object):
     def __init__(self, vim_dir):
-        self.vim_dir = vim_dir
+        self.vim_dir = vim_dir  # user's .vim dir path
+        self.manager = Manager(vim_dir)
 
     def remove_package(self, package_name):
-        file_path = os.path.join(self.vim_dir,
-                                 'vimapt/install',
-                                 package_name)
-        # print file_path
-        fd = open(file_path, 'r')
-        file_stream = fd.read()
-        fd.close()
-        meta_data = loads(file_stream)
-
-        for file_name in meta_data:
-            file_token = file_name.split("/")
-            if file_token[0] == "vimrc":
-                continue
-            target_path = os.path.join(self.vim_dir, file_name)
-
-            # print target_path
-            if os.path.isfile(target_path):
-                os.unlink(target_path)
-            else:
-                pass
-        remove_path = os.path.join(self.vim_dir,
-                                   'vimapt/remove',
-                                   package_name)
-        os.rename(file_path, remove_path)
+        self.manager.execute(ManageAction.UNINSTALL, package_name)
