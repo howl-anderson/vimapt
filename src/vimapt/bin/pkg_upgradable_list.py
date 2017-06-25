1b8b0548c1f70c8e2ebbd10439f5b842591c30ae
Xiaoquan Kong <xiaoquan.kong@ele.me>
2017-06-25 21:44:04 +0800

    Init commit for provide `upgrade` command complement

---
+++ src/vimapt/bin/pkg_upgradable_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+
+import sys
+import vim
+
+
+def main():
+    vim_dir = sys.argv[1]
+    install_dir = os.path.join(vim_dir, 'vimapt/install')
+    file_list = []
+
+    for f in os.listdir(install_dir):
+        if os.path.isfile(os.path.join(install_dir, f)) and not os.path.basename(f).startswith('.'):
+            file_list.append(f)
+
+    return file_list
+
+
+if __name__ == "__main__":
+    package_list = main()
+
+    # set vim's variable to make command complete function works
+    pkg_list_string = "[" + ",".join(["'" + i + "'" for i in package_list]) + "]"
+    vim.command('let s:potential_upgradable_package_list = ' + pkg_list_string)
